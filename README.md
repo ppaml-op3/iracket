a07f92d8580ed48f2008df139209654bc7b06baf
Theo Giannakopoulos <theo.giannakopoulos@baesystems.com>
2015-05-28 16:10:27 -0400

    more dependencies fixes

---
+++ README.md
@@ -0,0 +1,5 @@
+Install stuff in dependencies.md via raco.
+mkdir -p ~/.ipython/kernels/racket/
+cp kernel.json ~/.ipython/kernels/racket/
+
+ipython notebook
---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+raco pkg install git://github.com/greghendershott/sha#master
+# raco pkg install git://github.com/jeapostrophe/zeromq#master
+raco pkg install git://github.com/tgiannak/racket-zeromq#wait-on-fd
+# raco pkg install git://github.com/mordae/racket-libuuid/tree#master
+raco pkg install git://github.com/tgiannak/racket-libuuid#osx-compat
